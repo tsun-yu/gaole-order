3f728c78d1f4a8c8759614aea2a23946548cc0c5
tsun-yu <69957560+tsun-yu@users.noreply.github.com>
2024-07-23 13:57:53 +0800

    feat: layout adjustment

--- src/views/Rush6Order.vue
+++ src/views/Rush6Order.vue
@@ -34,21 +34,19 @@ const orderDisplay = computed(() => {
   <FilterComponent v-model:data="hasSelected"></FilterComponent>
   <div class="container">
     <div v-for="(item, i) of orderDisplay" :key="'order' + i">
-      <p>{{ i }}</p>
-      <ul>
-        <li
-          :class="{
-            isSelect: item2?.selected,
-            isStar5: item2.name[0] !== 's' && item2.name[0] !== 'c',
-            isStar4: item2.name.includes('s4') || item2.name.includes('cp'),
-            isStar3: item2.name.includes('s3')
-          }"
-          v-for="(item2, i2) of item"
-          :key="'orderr' + i2"
-        >
-          {{ item2.name }}
-        </li>
-      </ul>
+      <div
+        class="item"
+        :class="{
+          isSelect: item2?.selected,
+          isStar5: item2.name[0] !== 's' && item2.name[0] !== 'c',
+          isStar4: item2.name.includes('s4') || item2.name.includes('cp'),
+          isStar3: item2.name.includes('s3')
+        }"
+        v-for="(item2, i2) of item"
+        :key="'orderr' + i2"
+      >
+        {{ item2.name }}
+      </div>
     </div>
   </div>
 </template>
@@ -56,57 +54,62 @@ const orderDisplay = computed(() => {
 <style lang="scss" scoped>
 .container {
   margin-top: 1rem;
-  display: flex;
-  gap: 0.125rem;
-  overflow: auto;
+  overflow: hidden;
+  display: grid;
 
-  .isStar5 {
-    outline: 1px solid #0b57d0;
-
-    &.isSelect {
-      background-color: #d2e3fc;
-      color: #0b57d0;
-    }
-  }
-  .isStar4 {
-    &.isSelect {
-      background-color: #c2e7ff;
-    }
-  }
-
-  .isSelect {
-    background-color: rgba(32, 33, 36, 0.059);
-  }
+  display: grid;
+  gap: 0.25rem;
+  grid-template-columns: repeat(auto-fit, minmax(2.5rem, 1fr));
 
   div {
-    flex-shrink: 0;
-    p {
-      text-align: center;
-    }
+    // flex: 1 1;
 
-    ul {
-      list-style: none;
+    .item {
+      background-color: #fff;
+      padding: 0.125rem 0.25rem;
+      border-radius: 0.125rem;
+      margin-bottom: 0.125rem;
+      font-size: 0.5rem;
 
-      li {
-        background-color: var(--color-teal1);
-        background-color: #fff;
-        padding: 0.25rem;
-        margin-bottom: 0.125rem;
-        overflow: hidden;
-        font-size: 0.5rem;
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
+      width: 2.5rem;
+      width: max(2.5rem, 100%);
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
 
-        &:first-child {
-          border-top-left-radius: 0.5rem;
-          border-top-right-radius: 0.5rem;
+      &:first-child {
+        border-top-left-radius: 0.5rem;
+        border-top-right-radius: 0.5rem;
+      }
+
+      &:last-child {
+        border-bottom-left-radius: 0.5rem;
+        border-bottom-right-radius: 0.5rem;
+      }
+
+      &.isStar5 {
+        outline: 1px solid #0b57d0;
+
+        &.isSelect {
+          background-color: #d2e3fc;
+          color: #0b57d0;
+          outline: 2px solid #0b57d0;
         }
-
-        &:last-child {
-          border-bottom-left-radius: 0.5rem;
-          border-bottom-right-radius: 0.5rem;
+      }
+      &.isStar4 {
+        &.isSelect {
+          background-color: #d2e3fc;
         }
+      }
+
+      &.isStar3 {
+        &.isSelect {
+          background-color: #c2e7ff;
+        }
+      }
+
+      &.isSelect {
+        background-color: rgba(32, 33, 36, 0.059);
       }
     }
   }
