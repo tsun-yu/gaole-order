5bd9dccb953faaa17ad46a1839906861d70aaf00
tsun-yu <69957560+tsun-yu@users.noreply.github.com>
2024-08-27 09:08:00 +0800

    feat: dropdown style adjustment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,56 +15,72 @@ const selectedPart = ref('');
 </script>
 
 <template>
-  <div class="input__container">
-    <select v-model="selectedPart">
-      <option v-for="gen in genList" :key="gen" :value="gen">{{ gen }}</option>
-    </select>
-    <button @click="router.push(`/order/${selectedPart}`)">GO</button>
+  <div class="container">
+    <div class="input__wrap">
+      <select v-model="selectedPart">
+        <option v-for="gen in genList" :key="gen" :value="gen">{{ gen }}</option>
+      </select>
+      <button @click="router.push(`/order/${selectedPart}`)">
+        <v-icon name="ri-send-plane-2-line" scale="1" fill="#676767" />
+      </button>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.input__container {
-  width: 100%;
-  flex: 0 0 4rem;
-  border-radius: 2rem;
+.container {
   display: flex;
-  align-items: center;
   justify-content: center;
-  gap: 1rem;
-  padding-inline: 1rem;
-  margin-top: 10rem;
+  align-items: center;
+  height: 100dvh;
 
-  select {
-    border: none;
-    height: 2.75rem;
-    padding-inline: 1.5rem;
-    font-size: 1.25rem;
-    color: #555;
+  .input__wrap {
+    width: min(100%, 30rem);
+    padding: 0.5rem 1rem;
+    margin: auto;
+    border-radius: 4rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
     background-color: #eaf1fb;
-    border: none;
-    flex: 0 1 min(100%, 30rem);
-    border-radius: 1.5rem;
 
-    &::placeholder {
-      color: #ccc;
+    select {
+      border: none;
+      height: 2.75rem;
+      padding-inline: 1.5rem;
+      font-size: 1.25rem;
+      color: #555;
+      background-color: #eaf1fb;
+      border: none;
+      flex: 1 1;
+      border-radius: 1.5rem;
+      cursor: pointer;
+
+      &:focus {
+        outline: none;
+      }
+
+      option {
+        background-color: #fff;
+      }
     }
-  }
 
-  button {
-    flex: 0 0 2.75rem;
-    height: 2.75rem;
-    border: none;
-    cursor: pointer;
-    border-radius: 50%;
-    background-color: #c4eed0;
-    font-size: 1rem;
-    color: #555;
-    display: grid;
-    place-items: center;
+    button {
+      flex: 0 0 2.75rem;
+      height: 2.75rem;
+      border: none;
+      cursor: pointer;
+      border-radius: 50%;
+      background-color: #eaf1fb;
+      font-size: 1rem;
+      color: #555;
+      display: grid;
+      place-items: center;
 
-    &:active {
-      background-color: #bce1c7;
+      &:hover {
+        background-image: linear-gradient(#0000001a, #0000001a);
+      }
     }
   }
 }
